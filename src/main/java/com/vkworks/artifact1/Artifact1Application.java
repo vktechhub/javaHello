0eccfc79b4e525f9c99322ff9d1f3d41a097da78
Vigneshkanna <vigneshkanna1985@gmail.com>
2025-06-05 16:11:39 +0530

    Initial version

---
+++ src/main/java/com/vkworks/artifact1/Artifact1Application.java
@@ -0,0 +1,15 @@
+package com.vkworks.artifact1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Artifact1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Artifact1Application.class, args);
+
+		System.out.println("This is Artifact1");
+	}
+
+}
---
+++ src/test/java/com/vkworks/artifact1/Artifact1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkworks.artifact1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Artifact1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
